18f0492e668139b974da011c9988f0b715c88c34
Aamosh1 <aamosh@waterflow.technology>
2023-12-22 12:46:40 +0545

    feat: Implemented direct storage to MongoDB for extracted data

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
+from pymongo import MongoClient
 
 
 class WebScraper:
@@ -16,7 +17,16 @@ class WebScraper:
         self.options.add_argument("window-size=1920,1080")
         self.driver = webdriver.Chrome(options=self.options)
         self.wait = WebDriverWait(self.driver, 10)
+        self.client = MongoClient("mongodb+srv://aamoshmaharjan234:QOpFnRrwRCaJjzBd@cluster0.1cjd14y.mongodb.net/")  # Update with your MongoDB connection string
+        self.db = self.client["webScrap"] # Update with your MongoDB database name
 
+    def connect_to_mongo(self):
+        # Connect to MongoDB and return the collection
+        return self.db["bank_collection"] # You can change according to your collection name
+
+    def insert_into_mongo(self, data):
+        collection = self.connect_to_mongo()
+        collection.insert_one(data)
 
     def navigate_to_page(self, url):
         self.driver.get(url)
@@ -45,12 +55,12 @@ class WebScraper:
             bank_data_single = self.bank_data()
             data["data"].extend(bank_data_single)
             final_data.append(data)
+            self.insert_into_mongo(data)
             print(f'Extracted From {bank_name}')
-            if index < len(bank_options)-1:
+            if index < len(bank_options) - 1:
                 Bank_select = Select(self.driver.find_element(By.ID, "exampleFormControlSelect1"))
                 Bank_select.select_by_index(index + 1)
-        with open(self.file_name, 'w') as json_file:
-            json.dump(final_data, json_file, indent=4)
+
 
     def bank_data(self):
         whole_data = []
